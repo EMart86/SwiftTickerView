4b6964ff514b9d29b5f727314f34fbe855c6be75
Martin Eberl <martin.eberl@styria.com>
2019-03-03 01:39:36 +0100

    Added - InitialRenderer with closure for much more highly customizable content initialization - UpdateRenderer with closure for much more highly customizable content updates - UpdateRenderer bottomToTopStopAtCenter: this option has been used by many modern newstickers on tv, where the content comes in frim the bottom, stops at the center and leaves at the top. The time of which the content is being hold at the center is completely customizable

--- SwiftTickerView/Classes/Renderer.swift
+++ SwiftTickerView/Classes/Renderer.swift
@@ -25,6 +25,17 @@ open class Renderer: SwiftTickerContentRenderer {
     }
     
     /**
+     Add condition decorator for customizing the condition to do something at a certain point
+     
+     - Parameter condition: A tuple with the first iteme being a closure that indicates the condition and a second item beind a closure that indicates the action
+     */
+    open func customize(with condition: (condition: Condition, then: Action)) -> Renderer {
+        when = when ?? []
+        when?.append(condition)
+        return self
+    }
+    
+    /**
      Renders the content from right to left and centered vertically in the ticker view, starting at the right border.
      */
     public static var rightToLeft = Renderer(initials: [SwiftTickerItemDecorators.alignItemsLeftToEachOther(),
@@ -79,8 +90,77 @@ open class Renderer: SwiftTickerContentRenderer {
         current.frame.minY > tickerView.frame.maxY
     })
     
+    private class CustomTimer {
+        private var block: ((Timer) -> Void)? = nil
+        private weak var timer: Timer?
+        
+        func schedule(with interval: TimeInterval, repeats: Bool, block: @escaping ((Timer) -> Void)) {
+            if #available(iOS 10.0, tvOS 10.0, *) {
+                Timer.scheduledTimer(withTimeInterval: interval, repeats: repeats, block: block)
+            } else {
+                self.block = block
+                timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(timerFired(_:)), userInfo: nil, repeats: repeats)
+            }
+        }
+        
+        @objc private func timerFired(_ sender: Any) {
+            guard let timer = timer else {
+                return
+            }
+            block?(timer)
+        }
+    }
+    
+    /**
+     Renders the content bottom to top with the first item startin out of the bounds and moving each item up.
+     When the current item reaches the center, it stops for given ammounts of seconds. Thereafter it continues.
+     This behaviour is ment to be used on modern news tickers
+     
+     - Parameter holdForSeconds: use this option to let the user see the ticker node when centered in the view for ammount of seconds. Value has to be positive
+     */
+    public static func bottomToTopStopAtCenter(holdForSeconds seconds: TimeInterval) -> Renderer {
+        return Renderer(initials: [SwiftTickerItemDecorators.prepareAtBottomOuterBorder(force: true),
+                                   SwiftTickerItemDecorators.prepareAtLeftInnerBorder()],
+                        updates: [SwiftTickerItemDecorators.updateY(-)],
+                        when: [(
+                            condition: DefaultConditionBehaviour(condition: { node, tickerView, behaviour in
+                                func isInVerticalCenter(node: UIView, tickerView: SwiftTickerView, allowedOffset: CGFloat = 5) -> Bool {
+                                    let tickerCenter = tickerView.bounds.midY
+                                    let nodeCenter = node.frame.minY + node.frame.height / 2
+                                    return abs(tickerCenter - nodeCenter) < 5
+                                }
+                                
+                                guard behaviour.nodeView != node else {
+                                    if !isInVerticalCenter(node: node, tickerView: tickerView) {
+                                        behaviour.nodeView = nil
+                                    }
+                                    return false
+                                }
+                                if isInVerticalCenter(node: node, tickerView: tickerView) {
+                                    behaviour.nodeView = node
+                                    return true
+                                }
+                                behaviour.nodeView = nil
+                                return false
+                            }),
+                            then: { _, tickerView in
+                                tickerView.stop()
+                                CustomTimer().schedule(with: max(0, seconds), repeats: false, block: { _ in
+                                    tickerView.start()
+                                })
+                                return .return
+                            }
+                            )],
+                        shouldAddNewNode: { current, _, _ in
+                            current.frame.maxY < 0
+        }, shouldRemoveNode: { current, tickerView in
+            current.frame.maxY < 0
+        })
+    }
+    
     private var initials: [InitialRenderer & SwiftTickerItemDecorator]
     private var updates: [UpdateRenderer & SwiftTickerItemDecorator]
+    private var when: [(condition: Condition, then: Action)]?
     private let shouldAddNewNode: ShouldAddNewNode
     private let shouldRemoveNode: ShouldRemoveNode
     private var last: UIView?
@@ -95,16 +175,33 @@ open class Renderer: SwiftTickerContentRenderer {
      */
     public init(initials: [InitialRenderer & SwiftTickerItemDecorator],
                 updates: [UpdateRenderer & SwiftTickerItemDecorator],
+                when: [(condition: Condition, then: Action)]? = nil,
                 shouldAddNewNode: @escaping ShouldAddNewNode,
                 shouldRemoveNode: @escaping ShouldRemoveNode) {
         self.initials = initials
         self.updates = updates
+        self.when = when
         self.shouldAddNewNode = shouldAddNewNode
         self.shouldRemoveNode = shouldRemoveNode
     }
-
+    
     open func tickerViewUpdate(_ tickerView: SwiftTickerView, render nodeView: UIView, offset: CGFloat) {
         updates.forEach { $0.updateWith(current: nodeView, offset: offset) }
+        guard let when = when else {
+            return
+        }
+        for whenCondition in when {
+            if whenCondition.condition.meets(nodeView: nodeView, tickerView: tickerView) {
+                switch whenCondition.then(self, tickerView) {
+                case .return:
+                    return
+                case .break:
+                    break
+                case .continue:
+                    continue
+                }
+            }
+        }
     }
     
     open func tickerViewShouldAddNext(_ tickerView: SwiftTickerView, current nodeView: UIView) -> Bool {
